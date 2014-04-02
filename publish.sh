c347b49f13e9141283b10d1015fc74f0eeee3a74
Beau Simensen <beau@dflydev.com>
2014-04-02 13:48:25 -0500

    New username.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sculpin generate --env=prod || ( echo "Could not generate the site" && exit )
-rsync -avze 'ssh -p 668' output_prod/ offloc@pond000.reflxlabsinc.com:/opt/offloc/websites/reflxlabsinc.com || ( echo "Could not publish the site" && exit )
+rsync -avze 'ssh -p 668' output_prod/ reflxlabs@pond000.reflxlabsinc.com:/opt/reflxlabs/websites/reflxlabsinc.com || ( echo "Could not publish the site" && exit )
